d5df333dc46970aac85e35b20e855fdb0ad7c319
mahjouba bouchouf <45915936+manomano500@users.noreply.github.com>
2024-07-04 01:57:19 +0200

    v

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "./assets/tailwind.css";
-import "./assets/css/cstm.css";
+import App from "@/App.vue";
+import router from "@/router";
+import store from "@/store";
+import "@/assets/tailwind.css";
+import "@/assets/css/cstm.css";
 createApp(App).use(store).use(router).mount("#app");
